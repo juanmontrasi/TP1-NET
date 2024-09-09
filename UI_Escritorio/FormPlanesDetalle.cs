efe5637e335e3d93feae2af420b296fd44425d93
BrunoIglina <brunitoiglina@gmail.com>
2024-09-09 12:09:09 -0300

    Funcionalidad de Planes

---
+++ UI_Escritorio/FormPlanesDetalle.cs
@@ -0,0 +1,94 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_Escritorio
+{
+    public partial class FormPlanesDetalle : Form
+    {
+        private Plan plan;
+
+        public Plan Plan
+        {
+            get { return plan; }
+            set
+            {
+                plan = value;
+                if (plan != null)
+                {
+                    this.SetPlan();
+                }
+            }
+        }
+
+        public async void PlanesDetalle_Load(object sender, EventArgs e)
+        {
+
+        }
+        public FormPlanesDetalle()
+        {
+            InitializeComponent();
+        }
+        public bool EditMode { get; set; } = false;
+
+         private async void aceptarButton_Click(object sender, EventArgs e)
+        {
+            if (this.ValidatePlan())
+            {
+                if (plan != null)
+                {
+                    this.Plan.Nombre_Plan = nombreTextBox.Text;
+
+                    if (this.EditMode)
+                    {
+                        await PlanesApi.UpdateAsync(this.Plan);
+                    }
+                    else
+                    {
+                        await PlanesApi.AddAsync(this.Plan);
+                    }
+
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("El Plan no está inicializado.");
+                }
+            }
+        }
+
+        private void cancelarButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SetPlan()
+        {
+            if (this.Plan != null)
+            {
+                nombreTextBox.Text = this.Plan.Nombre_Plan;
+            }
+        }
+
+        private bool ValidatePlan()
+        {
+            bool isValid = true;
+            errorProvider.SetError(nombreTextBox, string.Empty);
+
+            if (this.nombreTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(nombreTextBox, "El Nombre es Requerido");
+            }
+
+            return isValid;
+        }
+    }
+}
---
+++ proyecto_academia/Servicios/PlanesService.cs
@@ -0,0 +1,61 @@
+﻿using Entidades;
+using proyecto_academia.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using proyecto_academia.Context;
+
+namespace proyecto_academia.Servicios
+{
+    public class PlanesServices
+    {
+        public void Add(Plan plan)
+        {
+            using var context = new AcademiaDbContext();
+
+            context.Planes.Add(plan);
+            context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            using var context = new AcademiaDbContext();
+
+            Plan? planToDelete = context.Planes.Find(id);
+
+            if (planToDelete != null)
+            {
+                context.Planes.Remove(planToDelete);
+                context.SaveChanges();
+            }
+        }
+
+        public Plan? Get(int id)
+        {
+            using var context = new AcademiaDbContext();
+
+            return context.Planes.Find(id);
+        }
+
+        public IEnumerable<Plan> GetAll()
+        {
+            using var context = new AcademiaDbContext();
+
+            return context.Planes.ToList();
+        }
+
+        public void Update(Plan plan)
+        {
+            using var context = new AcademiaDbContext();
+
+            Plan? planToUpdate = context.Planes.Find(plan.IdPlan);
+
+            if (planToUpdate != null)
+            {
+                planToUpdate.Nombre_Plan = plan.Nombre_Plan;
+                context.SaveChanges();
+            }
+        }
+    }
+}
